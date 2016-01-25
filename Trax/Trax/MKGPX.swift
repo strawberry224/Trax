f01fc46aedb17a0daa1c0ab6df718f3fa3ea590e
strawberry224 <286966170@qq.com>
2016-01-25 23:05:26 +0800

    Stanford cs193p 14

---
+++ Trax/Trax/MKGPX.swift
@@ -0,0 +1,31 @@
+//
+//  MKGPX.swift
+//  Trax
+//
+//  Created by Shen Lijia on 16/1/25.
+//  Copyright © 2016年 ShenLijia. All rights reserved.
+//
+
+import MapKit
+
+extension GPX.Waypoint: MKAnnotation {
+    var coordinate: CLLocationCoordinate2D {
+        return CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+    }
+    
+    var title: String? { return name }
+    
+    var subtitle: String? { return info }
+    
+    var thumbnailURL: NSURL? { return getImageURLofType("thumbnail") }
+    var imageURL: NSURL? { return getImageURLofType("large") }
+    
+    private func getImageURLofType(type: String) -> NSURL? {
+        for link in links {
+            if link.type == type {
+                return link.url
+            }
+        }
+        return nil
+    }
+}
