af2de8529048e60a49cfe79d9ec86ef471bb4895
strawberry224 <286966170@qq.com>
2016-01-26 16:52:23 +0800

    Stanford cs193p 15

--- Trax/Trax/GPXViewController.swift
+++ Trax/Trax/GPXViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 import MapKit
 
-class GPXViewController: UIViewController, MKMapViewDelegate {
+class GPXViewController: UIViewController, MKMapViewDelegate, UIPopoverPresentationControllerDelegate {
     
 
     @IBOutlet weak var mapView: MKMapView! {
@@ -45,6 +45,8 @@ class GPXViewController: UIViewController, MKMapViewDelegate {
         static let LeftCalloutFrame = CGRect(x: 0, y: 0, width: 59, height: 59)
         static let AnnotationViewReuseIdentifier = "waypoint"
         static let ShowImageSegue = "Show Image"
+        static let EditWaypointSegue = "Edit Waypoint"
+        static let EditWaypointPopoverWidth: CGFloat = 320
     }
     
     func mapView(mapView: MKMapView, viewForAnnotation annotation: MKAnnotation) -> MKAnnotationView? {
@@ -57,13 +59,15 @@ class GPXViewController: UIViewController, MKMapViewDelegate {
             view!.annotation = annotation
         }
         
+        view!.draggable = annotation is EditableWaypoint
+        
         view!.leftCalloutAccessoryView = nil
         view!.rightCalloutAccessoryView = nil
         if let waypoint = annotation as? GPX.Waypoint {
             if waypoint.thumbnailURL != nil {
-                view!.leftCalloutAccessoryView = UIImageView(frame: Constants.LeftCalloutFrame)
+                view!.leftCalloutAccessoryView = UIButton(frame: Constants.LeftCalloutFrame)
             }
-            if waypoint.imageURL != nil {
+            if annotation is EditableWaypoint {
                 view!.rightCalloutAccessoryView = UIButton(type: UIButtonType.DetailDisclosure) as UIButton
             }
         }
@@ -71,27 +75,71 @@ class GPXViewController: UIViewController, MKMapViewDelegate {
         return view
     }
     
+    
+    @IBAction func addWaypoint(sender: UILongPressGestureRecognizer) {
+        if sender.state == UIGestureRecognizerState.Began {
+            let coordinate = mapView.convertPoint(sender.locationInView(mapView), toCoordinateFromView: mapView)
+            let waypoint = EditableWaypoint(latitude: coordinate.latitude, longitude: coordinate.longitude)
+            waypoint.name = "Dropped"
+            mapView.addAnnotation(waypoint)
+        }
+    }
+    
     func mapView(mapView: MKMapView, annotationView view: MKAnnotationView, calloutAccessoryControlTapped control: UIControl) {
-        performSegueWithIdentifier(Constants.ShowImageSegue, sender: view)
+        if (control as? UIButton)?.buttonType == UIButtonType.DetailDisclosure {
+            mapView.deselectAnnotation(view.annotation, animated: false)
+            performSegueWithIdentifier(Constants.EditWaypointSegue, sender: view)
+        } else if let waypoint = view.annotation as? GPX.Waypoint {
+            if waypoint.imageURL != nil {
+                performSegueWithIdentifier(Constants.ShowImageSegue, sender: view)
+            }
+        }
+        
     }
     
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         if segue.identifier == Constants.ShowImageSegue {
             if let waypoint = (sender as? MKAnnotationView)?.annotation as? GPX.Waypoint {
-                if let ivc = segue.destinationViewController as? ImageViewController {
+                if let ivc = segue.destinationViewController.contentViewController as? ImageViewController {
                     ivc.imageURL = waypoint.imageURL
                     ivc.title = waypoint.name
+                }
+            }
+        } else if segue.identifier == Constants.EditWaypointSegue {
+            if let waypoint = (sender as? MKAnnotationView)?.annotation as? EditableWaypoint {
+                if let ewvc = segue.destinationViewController.contentViewController as? EditWaypointViewController {
+                    if let ppc = ewvc.popoverPresentationController {
+                        let coordinatePoint = mapView.convertCoordinate(waypoint.coordinate, toPointToView: mapView)
+                        ppc.sourceRect = (sender as! MKAnnotationView).popoverSourceRectForCoordinatePoint(coordinatePoint)
+                        let minimumSize = ewvc.view.systemLayoutSizeFittingSize(UILayoutFittingCompressedSize)
+                        ewvc.preferredContentSize = CGSize(width: Constants.EditWaypointPopoverWidth, height: minimumSize.height)
+                        ppc.delegate = self
+                    }
+                    ewvc.waypointToEdit = waypoint
                 }
             }
         }
     }
     
+    
+    func adaptivePresentationStyleForPresentationController(controller: UIPresentationController) -> UIModalPresentationStyle {
+        return UIModalPresentationStyle.OverFullScreen
+    }
+    
+    func presentationController(controller: UIPresentationController, viewControllerForAdaptivePresentationStyle style: UIModalPresentationStyle) -> UIViewController? {
+        let navcon =  UINavigationController(rootViewController: controller.presentedViewController)
+        let visualEffectView = UIVisualEffectView(effect: UIBlurEffect(style: .ExtraLight))
+        visualEffectView.frame = navcon.view.bounds
+        navcon.view.insertSubview(visualEffectView, atIndex: 0)
+        return navcon
+    }
+    
     func mapView(mapView: MKMapView, didSelectAnnotationView view: MKAnnotationView) {
         if let waypoint =  view.annotation as? GPX.Waypoint {
-            if let thumbnailImageView  = view.leftCalloutAccessoryView as? UIImageView {
+            if let thumbnailImageButton = view.leftCalloutAccessoryView as? UIButton {
                 if let imageData = NSData(contentsOfURL: waypoint.thumbnailURL!) {
                     if let image = UIImage(data: imageData) {
-                        thumbnailImageView.image = image
+                        thumbnailImageButton.setImage(image, forState: .Normal)
                     }
                 }
             }
@@ -112,8 +160,27 @@ class GPXViewController: UIViewController, MKMapViewDelegate {
             }
         }
         
-        //gpxURL = NSURL(string: "http://cs193p.stanford.edu/Vacation.gpx")
+        gpxURL = NSURL(string: "http://cs193p.stanford.edu/Vacation.gpx")
     }
 
+}
+
+extension UIViewController {
+    var contentViewController: UIViewController {
+        if let navcon = self as? UINavigationController {
+            return navcon.visibleViewController!
+        } else {
+            return self
+        }
+    }
+}
+
+extension MKAnnotationView {
+    func popoverSourceRectForCoordinatePoint(coordinatePoint: CGPoint) -> CGRect {
+        var popoverSourceRectCenter = coordinatePoint
+        popoverSourceRectCenter.x -= frame.width / 2 - centerOffset.x - calloutOffset.x
+        popoverSourceRectCenter.y -= frame.height / 2 - centerOffset.y - calloutOffset.y
+        return CGRect(origin: popoverSourceRectCenter, size: frame.size)
+    }
 }
 
--- Trax/Trax/ImageViewController.swift
+++ Trax/Trax/ImageViewController.swift
@@ -1,21 +1,21 @@
 //
 //  ImageViewController.swift
-//  Cassini
+//  Trax
 //
-//  Created by Shen Lijia on 16/1/13.
+//  Created by Shen Lijia on 16/1/22.
 //  Copyright © 2016年 ShenLijia. All rights reserved.
 //
 
 import UIKit
 
-class ImageViewController: UIViewController, UIScrollViewDelegate {
-    
+class ImageViewController: UIViewController, UIScrollViewDelegate
+{
     // our Model
     // publicly settable
     // when it changes (but only if we are on screen)
-    // we'll fetch the image from the imageURL
+    //   we'll fetch the image from the imageURL
     // if we're off screen when this happens (view.window == nil)
-    // viewWillAppear will get it for us later
+    //   viewWillAppear will get it for us later
     var imageURL: NSURL? {
         didSet {
             image = nil
@@ -25,24 +25,23 @@ class ImageViewController: UIViewController, UIScrollViewDelegate {
         }
     }
     
-    @IBOutlet weak var spinner: UIActivityIndicatorView!
-    
     // fetches the image at imageURL
     // does so off the main thread
     // then puts a closure back on the main queue
-    // to handle putting the image in the UI
-    // (since we aren't allowed to do UI anywhere but main queue)
-    private func fetchImage() {
+    //   to handle putting the image in the UI
+    //   (since we aren't allowed to do UI anywhere but main queue)
+    private func fetchImage()
+    {
         if let url = imageURL {
             spinner?.startAnimating()
             let qos = Int(QOS_CLASS_USER_INITIATED.rawValue)
             dispatch_async(dispatch_get_global_queue(qos, 0)) { () -> Void in
-                let imageData = NSData(contentsOfURL: url) // this blocks the thread it isi ob
+                let imageData = NSData(contentsOfURL: url) // this blocks the thread it is on
                 dispatch_async(dispatch_get_main_queue()) {
                     // only do something with this image
                     // if the url we fetched is the current imageURL we want
                     // (that might have changed while we were off fetching this one)
-                    if url == self.imageURL {
+                    if url == self.imageURL { // the variable "url" is capture from above
                         if imageData != nil {
                             // this might be a waste of time if our MVC is out of action now
                             // which it might be if someone hit the Back button
@@ -58,12 +57,14 @@ class ImageViewController: UIViewController, UIScrollViewDelegate {
         }
     }
     
-    @IBOutlet weak var scrollView: UIScrollView! {
+    @IBOutlet private weak var spinner: UIActivityIndicatorView!
+    
+    @IBOutlet private weak var scrollView: UIScrollView! {
         didSet {
-            scrollView.contentSize = imageView.frame.size   // critical to set this!
-            scrollView.delegate = self                      // required for zooming
-            scrollView.minimumZoomScale = 0.03              // required for zooming
-            scrollView.maximumZoomScale = 1.0               // required for zooming
+            scrollView.contentSize = imageView.frame.size // critical to set this!
+            scrollView.delegate = self                    // required for zooming
+            scrollView.minimumZoomScale = 0.03            // required for zooming
+            scrollView.maximumZoomScale = 1.0             // required for zooming
         }
     }
     
@@ -72,13 +73,14 @@ class ImageViewController: UIViewController, UIScrollViewDelegate {
     func viewForZoomingInScrollView(scrollView: UIScrollView) -> UIView? {
         return imageView
     }
+    
     private var imageView = UIImageView()
     
     // convenience computed property
     // lets us get involved every time we set an image in imageView
-    // we can do things like resize the imageView
-    // set the scroll view's contentSize
-    // and stop the spinner
+    // we can do things like resize the imageView,
+    //   set the scroll view's contentSize,
+    //   and stop the spinner
     private var image: UIImage? {
         get { return imageView.image }
         set {
@@ -91,13 +93,10 @@ class ImageViewController: UIViewController, UIScrollViewDelegate {
     
     // put our imageView into the view hierarchy
     // as a subview of the scrollView
-    // (will install it into the content area of the scroll ciew)
+    // (will install it into the content area of the scroll view)
     override func viewDidLoad() {
         super.viewDidLoad()
         scrollView.addSubview(imageView)
-        //        if image == nil {
-        //            imageURL = DemoURL.TFboys
-        //        }
     }
     
     // for efficiency, we will only actually fetch the image
